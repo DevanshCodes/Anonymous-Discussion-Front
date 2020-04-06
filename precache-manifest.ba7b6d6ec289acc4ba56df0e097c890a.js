a03ff68e2b6c6eebe529ba4df35eeda82025411c
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-04-06 15:29:03 +0530

    Updates

---
+++ precache-manifest.ba7b6d6ec289acc4ba56df0e097c890a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b60a600fb72ae09c2adb0fb11b39ac6",
+    "url": "/anonymous-project-frontend/index.html"
+  },
+  {
+    "revision": "d9b79c74e9c26579d006",
+    "url": "/anonymous-project-frontend/static/css/main.46039ddf.chunk.css"
+  },
+  {
+    "revision": "1e9cafa28785cf416153",
+    "url": "/anonymous-project-frontend/static/js/2.2964d9e0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/anonymous-project-frontend/static/js/2.2964d9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b79c74e9c26579d006",
+    "url": "/anonymous-project-frontend/static/js/main.69ea4e63.chunk.js"
+  },
+  {
+    "revision": "d71925e88746755fcdad",
+    "url": "/anonymous-project-frontend/static/js/runtime-main.a40f67ce.js"
+  }
+]);
