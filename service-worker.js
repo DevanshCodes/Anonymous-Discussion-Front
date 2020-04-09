5d22e8bc13a8569f1485665d8dededad68931f70
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-04-09 19:41:06 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anonymous-project-frontend/precache-manifest.da322f86abc73088850886394a9b07f4.js"
+  "/anonymous-project-frontend/precache-manifest.d325076757a3bd23bba69f81b428c779.js"
 );
 
 self.addEventListener('message', (event) => {
