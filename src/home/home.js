83f1b69b2ae4b20242a73a773659eeb1bbcbd321
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-04-19 20:22:02 +0530

    added the github link and styles

--- src/home/home.js
+++ src/home/home.js
@@ -1,69 +1,99 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./home.scss";
-import { Card, CardContent, Container, Avatar} from '@material-ui/core';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, InputGroup, FormControl } from 'react-bootstrap';
+import { Card, CardContent, Container, Avatar } from "@material-ui/core";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {ReactComponent as Logo} from "../github.svg";
+import { Button, InputGroup, FormControl } from "react-bootstrap";
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            roomno: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      roomno: "",
+    };
+    this.handleChangeusername = this.handleChangeusername.bind(this);
+    this.handleChangeroomno = this.handleChangeroomno.bind(this);
+  }
 
-        };
-        this.handleChangeusername = this.handleChangeusername.bind(this)
-        this.handleChangeroomno = this.handleChangeroomno.bind(this);
-    }
+  handleChangeusername(event) {
+    this.setState({ username: event.target.value });
+  }
+  handleChangeroomno(event) {
+    this.setState({ roomno: event.target.value });
+  }
 
-    handleChangeusername(event) {
-        this.setState({ username: event.target.value })
-    }
-    handleChangeroomno(event) {
-        this.setState({ roomno: event.target.value })
-    }
-
-    render() {
-        return (
-            <div className='main'>
-                <Container maxWidth="md">
-                    <h1>Anonymous Discussion!</h1>
-                </Container>
-                <Card className="Card">
-                    <CardContent className="content">
-                        <div className="greeting">Let's Get Started!</div>
-                        <div><Avatar className="avatar"></Avatar></div>
-                        <form className="form">
-                            <InputGroup className="mb-3" type="text" value={this.state.username} id="inputText" onChange={this.handleChangeusername}>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl
-                                    placeholder="Username"
-                                    aria-label="Username"
-                                    aria-describedby="basic-addon1"
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3" type="text" value={this.state.roomno} id="inputText" onChange={this.handleChangeroomno}>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="basic-addon1">#</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl
-                                    placeholder="RoomID"
-                                    aria-label="Username"
-                                    aria-describedby="basic-addon1"
-                                />
-                            </InputGroup>
-                            <Link to={`${this.state.username}/${this.state.roomno}`}><Button className="Submit" type="submit" value="Submit" variant="primary"> Submit </Button></Link>
-                        </form>
-                    </CardContent>
-                </Card>
-                <Container maxWidth="md">
-                </Container>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="main">
+        <Container maxWidth="md" className="container">
+          <div>
+            <h1>Anonymous Discussion!</h1>
+          </div>
+            <Card className="Card">
+              <CardContent className="content">
+                <div className="greeting">Let's Get Started!</div>
+                <div>
+                  <Avatar className="avatar"></Avatar>
+                </div>
+                <form className="form">
+                  <InputGroup
+                    className="mb-3"
+                    type="text"
+                    value={this.state.username}
+                    id="inputText"
+                    onChange={this.handleChangeusername}
+                  >
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                      placeholder="Username"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                    />
+                  </InputGroup>
+                  <InputGroup
+                    className="mb-3"
+                    type="text"
+                    value={this.state.roomno}
+                    id="inputText"
+                    onChange={this.handleChangeroomno}
+                  >
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="basic-addon1">#</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                      placeholder="RoomID"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                    />
+                  </InputGroup>
+                  <Link to={`${this.state.username}/${this.state.roomno}`}>
+                    <Button
+                      className="Submit"
+                      type="submit"
+                      value="Submit"
+                      variant="primary"
+                    >
+                      {" "}
+                      Submit{" "}
+                    </Button>
+                  </Link>
+                </form>
+              </CardContent>
+            </Card>
+          </Container>
+          <div className="github">
+              <Logo className="logo"></Logo>
+            <a href="https://github.com/DevanshCodes" className="text">
+              DevanshCodes
+            </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
